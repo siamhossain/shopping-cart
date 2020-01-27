71a88c54f89f113acb85943b7c94b4291352aecd
Siam Hossain <siam.hossain1994@gmail.com>
2020-01-28 02:23:48 +0600

    Design cart item details

---
+++ cart.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cart</title>
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" integrity="sha256-46qynGAkLSFpVbEBog43gvNhfrOj+BmwXdxFgVK/Kvc=" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="bg-light">
+    
+<?php
+    require_once('php/header.php');
+?>
+
+<div class="container-fluid">
+    <div class="row px-5">
+        <div class="col-md-7">
+            <div class="shopping-cart">
+                <h6>My Cart</h6>
+                <hr>
+
+                <form action="cart.php" method="get" class="cart-items">
+                    <div class="border rounded">
+                        <div class="row bg-white">
+                            <div class="col-md-3 pl-0">
+                                <img src="./upload/product1.png" alt="Image1" class="img-fluid">
+                            </div>
+                            <div class="col-md-6">
+                                <h5 class="pt-2">Product1</h5>
+                                <small class="text-secondary">Seller:siam</small>
+                                <h5 class="pt-2">$599</h5>
+                                <button type="submit" class="btn btn-warning">Save for Later</button>
+                                <button type="submit" class="btn btn-danger mx-2" name="remove">Remove</button>
+                            </div>
+                            <div class="col-md-3 py-5">
+                                <div>
+                                    <button type="button" class="btn bg-light border rounded-circle"><i class="fas fa-minus"></i></button>
+                                    <input type="text" value="1" class="form-control w-25 d-inline">
+                                    <button type="button" class="btn bg-light border rounded-circle"><i class="fas fa-plus"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="col-md-5"></div>
+    </div>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
